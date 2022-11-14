fa09650da81d393e0fe3b77de13280f686b5023c
kimhana <hnone99@gmail.com>
2022-11-14 18:18:29 +0900

    레이아웃&가이드

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -4,7 +4,7 @@ let Common = {
   },
   common: function () {
     //datepicker
-    $('[data-picker="date"]').each(function () {
+    /* $('[data-picker="date"]').each(function () {
       $(this).datepicker({
         monthNames: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
         monthNamesShort: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
@@ -13,7 +13,42 @@ let Common = {
         showOtherMonths: true,
         changeMonth: true,
         changeYear: true,
-        dateFormat: "yy-mm-dd",
+        dateFormat: "yy.mm.dd",
+      });
+    }); */
+    var datePickerInputs = [];
+    $('[data-picker="date"]').each(function (i) {
+      // eslint-disable-next-line no-undef
+      datePickerInputs[i] = new tempusDominus.TempusDominus($(this)[0], {
+        display: {
+          theme: "dark",
+          viewMode: "calendar",
+        },
+      });
+      // eslint-disable-next-line no-undef
+      datePickerInputs[i].dates.formatInput = (date) => moment(date).format("YYYY.MM.DD");
+    });
+
+    //datetimepicker
+    var dateTimePickerInputs = [];
+    $('[data-picker="datetime"]').each(function (i) {
+      // eslint-disable-next-line no-undef
+      dateTimePickerInputs[i] = new tempusDominus.TempusDominus($(this)[0], {
+        display: {
+          sideBySide: true,
+          theme: "dark",
+        },
+      });
+      // eslint-disable-next-line no-undef
+      dateTimePickerInputs[i].dates.formatInput = (date) => moment(date).format("YYYY.MM.DD HH:mm");
+    });
+
+    //timepicker
+    $('[data-picker="color"]').each(function (i) {
+      $(this).spectrum({
+        type: "component",
+        showPalette: true,
+        togglePaletteOnly: true,
       });
     });
   },
@@ -21,7 +56,7 @@ let Common = {
 
 let Aside = {
   init: function () {
-    this.nav();
+    //this.nav();
   },
   nav: function () {
     $(".sidebar-nav > li > a").on("click", function (e) {
